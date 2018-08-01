b54b345d1cd13f02e559acb75de20f480c5083fe
yanmingjie0223 <2386068693@qq.com>
2018-08-01 22:44:04 +0800

    [BASE] [ADD] StringUtils.js工具中添加将字符串序列化为Unicode和反序列化，避开emoji表情

--- BaseClassES6/src/core/utils/StringUtils.js
+++ BaseClassES6/src/core/utils/StringUtils.js
@@ -28,7 +28,7 @@ class StringUtils extends BaseClass {
             let s = strArr[i];
             if (this.isChinese(s)) {
                 length += 2;
-            } 
+            }
             else {
                 length += 1;
             }
@@ -44,6 +44,44 @@ class StringUtils extends BaseClass {
     isChinese(str) {
         let reg = /^.*[\u4E00-\u9FA5]+.*$/;
         return reg.test(str);
+    }
+
+    /**
+     * 将字符串（emoji表情）转换成16进制，Unicode编码存储是4个字节，两个2个字节组合
+     *
+     * @param {string} str
+     */
+    stringToCode16(str) {
+        const len = str.length;
+        let codes = '';
+        for (let i = 0; i < len; i++) {
+            let code = str.charCodeAt(i).toString(16);
+            codes += ';' + code;
+        }
+        return codes;
+    }
+
+    /**
+     * 字节码，转换成unicode显示
+     *
+     * @param {string} str
+     */
+    code16ToString(str) {
+        let uCodeStr = '';
+        const strArr = str.split(';');
+        for (let i = 0, len = strArr.length; i < len; i++) {
+            let code = strArr[i];
+            // ASCII补充高位
+            let fillNum = 0;
+            if (code.length < 4) {
+                fillNum = 4 - code.length;
+            }
+            for (let k = 0; k < fillNum; k++) {
+                code = '0' + code;
+            }
+            uCodeStr += '\\u' + code;
+        }
+        return uCodeStr;
     }
 
 }
